7e7c02864c4f3ebe4613cccfa513867668d95b8b
tokhi <shafi.tokhi@gmail.com>
2019-10-24 16:26:05 +0200

    number weight order

---
+++ weight_order.rb
@@ -0,0 +1,19 @@
+def order_weight(str)
+  num_arr = str.split(' ')
+  output = {}
+  weight = 0
+  num_arr.sort.each do |n|
+    weight = 0
+    n.split('').each do |n1|
+      weight += n1.to_i
+    end
+    output[weight] = output.key?(weight) ?  output[weight] + " #{n}"  : output[weight] = n
+  end
+  (output.sort_by {|k,v| k}.to_h).values.join(' ')
+end
+
+
+
+# puts order_weight("56 65 74 100 99 68 86 180 90")
+# puts order_weight("2000 11 11 123 1234000 44444444 9999 10003 22")
+puts order_weight("56 65 74 100 99 68 86 90 180")
